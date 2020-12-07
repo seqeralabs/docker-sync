2a97ca5d5d8acefa0b473eb09a7ff5e30e772031
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2020-12-07 16:14:05 +0100

    Patch for ECR public reg

--- docker-sync.sh
+++ docker-sync.sh
@@ -1,12 +1,20 @@
-#!/bin/bash -eou 
+#!/bin/bash 
+set -e
+set -u
+set -x 
 
 repo=$1
-target=$2
+target=public.ecr.aws/e2x4u7r1
 
-for x in $(./docker-remote-tags.sh $1); do 
+tags=$(./docker-remote-tags.sh $1)
+sleep 5
+for x in $tags; do 
     echo "+ Sync $repo:$x -> $target"
+    docker login
     docker pull $repo:$x
-    docker tag $repo:$x $target/$repo:$x
-    docker push $target/$repo:$x
+    docker tag $repo:$x $target/nextflow:$x
+    ecr-login-public.sh
+    docker push $target/nextflow:$x
     echo ""
+    sleep 5
 done
