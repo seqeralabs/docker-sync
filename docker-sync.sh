7137871c7d811687ce0d375efadc445d595e1fd5
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2021-01-03 18:26:05 +0100

    WIP

--- docker-sync.sh
+++ docker-sync.sh
@@ -1,5 +1,4 @@
 #!/bin/bash 
-set -e
 set -u
 set -x 
 
@@ -7,14 +6,17 @@ repo=$1
 target=public.ecr.aws/e2x4u7r1
 
 tags=$(./docker-remote-tags.sh $1)
-sleep 5
 for x in $tags; do 
     echo "+ Sync $repo:$x -> $target"
-    docker login
-    docker pull $repo:$x
+    while [ true ]; do 
+        docker pull $repo:$x
+        if [[ $? == '0' ]]; then
+          break
+        else 
+          sleep 10
+        fi
+    done 
     docker tag $repo:$x $target/nextflow:$x
-    ecr-login-public.sh
     docker push $target/nextflow:$x
     echo ""
-    sleep 5
 done
